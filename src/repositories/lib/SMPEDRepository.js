43e7cdcab1b6d4019a033cd3a73f89516aea1ad2
Caio Ramos <caioramos97@gmail.com>
2018-10-17 15:50:14 -0300

    Tabs por espaços again #13
    Signed-off-by: Caio Ramos <caioramos97@gmail.com>

--- src/repositories/lib/SMPEDRepository.js
+++ src/repositories/lib/SMPEDRepository.js
@@ -4,22 +4,22 @@ const ROOT_URL = 'http://api-dev-smped.ml:3001';
 
 export class SMPEDRepository {
 
-	async get(endpoint) {
-		return await axios.get(`${ROOT_URL}/${endpoint}`);
-	}
+    async get(endpoint) {
+        return await axios.get(`${ROOT_URL}/${endpoint}`);
+    }
 
-	async post(endpoint, body) {
-		return await axios.post(`${ROOT_URL}/${endpoint}`, body);
-	}
+    async post(endpoint, body) {
+        return await axios.post(`${ROOT_URL}/${endpoint}`, body);
+    }
 
-	async post_with_token(endpoint, body, token) {
-		return await axios.post(
-			`${ROOT_URL}/${endpoint}`, 
-			body,
-			{ 
-				headers: {
-					'Authorization': `Bearer ${token}`
-				}
-			});
-	}
+    async post_with_token(endpoint, body, token) {
+        return await axios.post(
+            `${ROOT_URL}/${endpoint}`,
+            body,
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+    }
 }
