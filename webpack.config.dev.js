2b969c2e899717226c29de2e0816c16325cda703
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-10-20 13:34:45 -0300

    Arquivos diferentes de config de webpack para env e prod.

---
+++ webpack.config.dev.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const UglifyJS = require('uglifyjs-webpack-plugin');
+const plugins = [];
+const webpack = require('webpack');
+
+plugins.push(new webpack.optimize.UglifyJsPlugin({ compress: true }));
+plugins.push(new webpack.EnvironmentPlugin(["API_ROOT_URL"]));
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    './src/index'
+  ],
+  devtool: 'cheap-module-source-map',
+  output: {
+    path: path.join(__dirname, 'public/build'),
+    filename: 'bundle.min.js',
+    publicPath: '/public/'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel-loader'],
+      include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('dev')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ],
+    watch: false,
+    node: {
+        fs: 'empty'
+    }
+};
