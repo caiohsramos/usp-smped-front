a3a1f7f14484661b407411349de2de22f42c583c
Gabriely Pereira <gabriely.pereira@usp.br>
2018-10-25 09:10:14 -0300

    Mudança na localização dos controllers e remoção de alguns arquivos da common #17
    Signed-off-by: Caio Ramos <caioramos97@gmail.com>
    Signed-off-by: Gabriely Pereira <gabriely.pereira@usp.br>

--- src/pages/dashboard/components/Category.js
+++ src/pages/dashboard/components/Category.js
@@ -3,7 +3,6 @@ import { Row } from './Row';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const row = props => {
