74f35421642235e042e24f34743238529c75ab12
Aline Ioste <rodrigheri@yahoo.com.br>
2018-10-29 16:02:44 -0300

    convidar usuarios

--- src/common/lib/SelectButton.js
+++ src/common/lib/SelectButton.js
@@ -6,30 +6,30 @@ import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
 
 const SelectButton = props => {
-	const {handleOpen, handleClose, handleChange, userType, dropDownState} = props;
+    const { handleOpen, handleClose, handleChange, userType, dropDownState } = props;
 
-	return (
+    return (
         <div>
-        <FormControl className='select-form'>
-          <Select
-            open={dropDownState}
-            onClose={() => handleClose()}
-            onOpen={() => handleOpen()}
-            value={userType}
-            onChange={(e) => handleChange(e)}
-            inputProps={{
-              name: 'userType',
-              id: 'demo-controlled-open-select',
-            }}
-          >
-		  	<MenuItem value={0}>Selecione</MenuItem>
-            <MenuItem value={1}>Administrador</MenuItem>
-            <MenuItem value={2}>Digitador</MenuItem>
-            <MenuItem value={3}>Convidado</MenuItem>
-          </Select>
-        </FormControl>
+            <FormControl className='select-form'>
+                <Select
+                    open={dropDownState}
+                    onClose={() => handleClose()}
+                    onOpen={() => handleOpen()}
+                    value={userType}
+                    onChange={(e) => handleChange(e)}
+                    inputProps={{
+                        name: 'userType',
+                        id: 'demo-controlled-open-select',
+                    }}
+                >
+                    <MenuItem value={0}>Selecione</MenuItem>
+                    <MenuItem value={1}>Administrador</MenuItem>
+                    <MenuItem value={2}>Digitador</MenuItem>
+                    <MenuItem value={3}>Convidado</MenuItem>
+                </Select>
+            </FormControl>
         </div>
-	);
+    );
 };
 
 export { SelectButton };
